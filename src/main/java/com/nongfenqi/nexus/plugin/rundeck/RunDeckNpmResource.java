62e96fb117cb693f29626374934f190334ccf5bc
DarthHater <jeffryxtron@gmail.com>
2017-08-15 10:28:04 -0800

    Initial file

---
+++ src/main/java/com/nongfenqi/nexus/plugin/rundeck/RunDeckNpmResource.java
@@ -0,0 +1,10 @@
+package com.nongfenqi.nexus.plugin.rundeck;
+
+import org.sonatype.goodies.common.ComponentSupport;
+import org.sonatype.nexus.rest.Resource;
+
+public class RunDeckNpmResource
+    extends ComponentSupport
+    implements Resource
+{
+}
